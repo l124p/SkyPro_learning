4eb8e5678591b70a62a9df3a54babf25f72f9265
Andry <andry.peshko@gmail.com>
2024-01-20 09:32:52 +0300

    add hw 7

---
+++ hw7/main.py
@@ -0,0 +1,26 @@
+def show_fileld():
+    """выводит игровое поле"""
+    pass
+
+def parse_input():
+    """Делит ввод пользователя на категорию и число"""
+    pass
+
+def show_question():
+    """Печатает вопрос"""
+    pass
+
+def show_stats():
+    """Выводит статистику"""
+    pass
+
+def save_results_to_file():
+    """Записывает резулбтаты в JSON файл"""
+    pass
+
+def main():
+
+    for i in range(5):
+        show_fileld()
+        parse_input()
+        show_question()
