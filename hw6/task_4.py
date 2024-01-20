616a014ada0bed27d32977c8e3a883ff603d1ec1
Andry <andry.peshko@gmail.com>
2024-01-20 08:46:21 +0300

    add first_course branch

--- hw6/task_4.py
+++
@@ -1,8 +0,0 @@
-from  validators import *
-
-pin = input('Pin')
-password = input('Password')
-email = input('email')
-name = input('Name')
-
-print(check_pin(pin), check_pass(password),checkjnail(email),check_name(name))
